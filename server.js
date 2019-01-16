5553053a1ef7c42f2f21cc57b339d11f1b27260a
Budi Saputra <kursdemy.budisaputra@gmail.com>
2019-01-16 13:30:25 +0700

    Template Engine #1

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var ejs = require("ejs");
+var engine = require("ejs-mate");
+
+var app = express();
+
+// Setup Engine
+app.engine("ejs", engine);
+app.set("view engine", "ejs" );
+
+// Setup middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Home
+app.get("/", function(req, res, next){
+    res.render("home");
+});
+
+// Server
+app.listen(3000);
